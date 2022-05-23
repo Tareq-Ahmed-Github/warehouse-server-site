6ce0a5d722442d74d140bc77f96f612dca7bc2a9
unknown <tareqrobin57@gmail.com>
2022-05-23 15:47:40 +0600

    server connected successfully

--- index.js
+++ index.js
@@ -8,27 +8,28 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 //deploy to heroku
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pdqpx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v8dty.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri)
 async function run() {
     try {
         await client.connect();
-        const itemsCollection = client.db('cycleWarehouse').collection('items');
-        const orderCollection = client.db('cycleWarehouse').collection('order');
-        app.get('/items', async (req, res) => {
+        const itemsCollection = client.db('mybikes').collection('bikes');
+        const orderCollection = client.db('mybikes').collection('order');
+        app.get('/bikes', async (req, res) => {
             const query = {};
             const cursor = itemsCollection.find(query);
             const items = await cursor.toArray();
             res.send(items);
         });
-        app.get('/items/:id', async (req, res) => {
+        app.get('/bikes/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const item = await itemsCollection.findOne(query);
             res.send(item);
         })
         //put
-        app.put('/items/:id', async (req, res) => {
+        app.put('/bikes/:id', async (req, res) => {
             const id = req.params.id;
             const updateItems = req.body;
             const filter = { _id: ObjectId(id) };
@@ -43,13 +44,13 @@ async function run() {
             res.send(answer);
         })
         //post
-        app.post('/items', async (req, res) => {
+        app.post('/bikes', async (req, res) => {
             const newItems = req.body;
             const result = await itemsCollection.insertOne(newItems);
             res.send(result)
         })
         //delete
-        app.delete('/items/:id', async (req, res) => {
+        app.delete('/bikes/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await itemsCollection.deleteOne(query);
